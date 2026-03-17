c71de6b3e343dc4d4990160f46e2fca6cd911210
Syam Sunder <vurumasyamsunder@gmail.com>
2026-03-17 07:36:59 +0530

    done by Syam

--- Money Expenses/Interfaces/IUser.cs
+++ Money Expenses/Interfaces/IUser.cs
@@ -4,13 +4,18 @@ using System.Collections.Generic;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using Money_Expenses.Interfaces;
 
 namespace Money_Expenses.Interfaces
 {
-    public class IUser
+    public interface IUser
     {
+        internal List<userRightsDTO> GetUserRights(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
 
-      
+        List<userRightsDTO> GetUserRights(string con, int userId);
 
 
 
